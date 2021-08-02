a309ee6d2e4e5840f88d0f3980f71ccb7cc48f5d
jackie gurung <jamyang463gelek@gmail.com>
2021-08-02 20:45:00 +0545

    Remove icon added

--- all/src/components/resRegister.js
+++ all/src/components/resRegister.js
@@ -459,8 +459,16 @@ export default class resRegister extends Component {
               </div>
               <div className="signaturePhoto">
                 <h3>Signature Photo:</h3>
+                <label htmlFor="signaturePhoto-upload">
+                  <div className="signature-upload">
+                  <svg height="25pt" width="25pt" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-plus" class="svg-inline--fa fa-user-plus fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M624 208h-64v-64c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v64h-64c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h64v64c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-64h64c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400 48c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
+                  <span>Upload Image</span>
+                  </div>
+                </label>
                 <input
                   type="file"
+                  id="signaturePhoto-upload"
+                  style={{display:"none"}}
                   value={this.state.input}
                   onChange={this.handleFileInputChange}
                 />
@@ -505,12 +513,23 @@ export default class resRegister extends Component {
                   {this.state.previewMultiple.length === 4 ? (
                     alert("You can add 4 images only.")
                   ) : (
-                    <input
-                      type="file"
-                      className="item1"
-                      // value={this.state.input}
-                      onChange={this.handleMultipleImage}
-                    />
+                    <>
+                      <label htmlFor="file-upload">
+                        <div className="upload-MultiIMAGE">
+                        <svg height="25pt" viewBox="0 -18 512 512" width="25pt" xmlns="http://www.w3.org/2000/svg"><path d="m432 0h-352c-44.113281 0-80 35.886719-80 80v280c0 44.113281 35.886719 80 80 80h190c7.628906 0 14.59375-4.339844 17.957031-11.191406 3.359375-6.847656 2.53125-15.015625-2.140625-21.046875l-52.3125-67.609375 144.992188-184.425782 93.503906 111.546876v33.726562c0 11.046875 8.953125 20 20 20s20-8.953125 20-20v-221c0-44.113281-35.886719-80-80-80zm-38.671875 111.152344c-3.871094-4.617188-9.609375-7.253906-15.640625-7.148438-6.027344.09375-11.6875 2.898438-15.410156 7.636719l-154.015625 195.894531-52.445313-67.773437c-3.789062-4.898438-9.628906-7.761719-15.816406-7.761719-.007812 0-.019531 0-.027344 0-6.199218.007812-12.046875 2.890625-15.824218 7.804688l-44.015626 57.21875c-6.734374 8.757812-5.097656 21.3125 3.65625 28.046874 8.757813 6.738282 21.3125 5.097657 28.046876-3.65625l28.210937-36.671874 89.1875 115.257812h-149.234375c-22.054688 0-40-17.945312-40-40v-280c0-22.054688 17.945312-40 40-40h352c22.054688 0 40 17.945312 40 40v125.007812zm-253.328125-39.152344c-33.085938 0-60 26.914062-60 60s26.914062 60 60 60 60-26.914062 60-60-26.914062-60-60-60zm0 80c-11.027344 0-20-8.972656-20-20s8.972656-20 20-20 20 8.972656 20 20-8.972656 20-20 20zm372 229c0 11.046875-8.953125 20-20 20h-55v55c0 11.046875-8.953125 20-20 20s-20-8.953125-20-20v-55h-55c-11.046875 0-20-8.953125-20-20s8.953125-20 20-20h55v-55c0-11.046875 8.953125-20 20-20s20 8.953125 20 20v55h55c11.046875 0 20 8.953125 20 20zm0 0"/></svg>
+                        <span>Upload Image</span>
+                        
+                        </div>
+                      
+                      </label>
+                      <input
+                        id="file-upload"
+                        type="file"
+                        style={{display:"none"}}
+                        // value={this.state.input}
+                        onChange={this.handleMultipleImage}
+                      />
+                    </>
                   )}
 
                   {this.state.previewMultiple &&
@@ -524,7 +543,7 @@ export default class resRegister extends Component {
                           height="200px"
                           width="300px"
                         />
-                        <button onClick={()=>{this.removeIMAGE(img)}}>remove</button>
+                        <button onClick={()=>{this.removeIMAGE(img)}}><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" class="svg-inline--fa fa-times-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path></svg></button>
                         </div>
                       );
                     })}
