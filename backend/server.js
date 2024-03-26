130a67fcbe02a58f77556ae372b5b3383f2f7dc1
ayush7aryal <ayush7aryal@gmail.com>
2024-03-26 14:14:31 +0545

    v

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ app.get("/", (req, res) => {
   res.json({ msg: "Dashboard!" });
 });
 
-app.listen(3000, () => {
-  console.log("Running on server 3000!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
